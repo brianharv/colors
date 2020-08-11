6f3dabee0e410d8bd50506debfc9a196d942374f
Brian Harvey <brian.harv3y@gmail.com>
2020-08-11 11:13:43 -0700

    complete practice lesson

--- js/scripts.js
+++ js/scripts.js
@@ -1,14 +1,17 @@
 $(document).ready(function() {
   $("button#green").click(function() {
+    $("body").removeClass();
     $("body").addClass("green-background");
   });
 
   $("button#yellow").click(function() {
+    $("body").removeClass();
     $("body").addClass("yellow-background");
   });
 
   $("button#red").click(function() {
-    $("body").addClass("yellow-background");
+    $("body").removeClass();
+    $("body").addClass("red-background");
   });
   
 })
